0c4b4bc8e64de7e19684daea6a9bad0d24ff94f3
valeroman <valeraromanovo@gmail.com>
2023-05-16 12:46:34 +0300

    Move all code into Gameboard

--- script.js
+++ script.js
@@ -1,30 +1,42 @@
 const Gameboard = function() {
+  let currentSign = 'X';
+  function changeCurrentSign() {
+    if (currentSign === 'X') currentSign = 'O';
+    if (currentSign === 'O') currentSign = 'X';
+  }
+  function showCurrentSign(){
+    return currentSign;
+  }
+
+  const gameBoardArr =  ['','','','','','','','',''];
+
+  const renderBoard = function() {
+    const BOARD = document.querySelector('#gamePad');
+  
+    let fillBoard = function(arr) {
+      BOARD.innerHTML = '';
+      for  (let i = 0; i < arr.length; i++) {
+        let item = document.createElement('div');
+        item.id = [i];
+        item.innerText = arr[i];
+        item.onclick = putSign.bind(null, item.id);
+        BOARD.appendChild(item);
+      }
+    }(gameBoardArr);  
+  
+  };
+
+  function putSign(id) {
+    if (gameBoardArr[id] === '') {    
+      gameBoardArr[id] = showCurrentSign();
+      changeCurrentSign();
+      renderBoard();
+    }
+  }
+
   return {
-    gameBoardArr: ['','','','','','','','',''],
+    renderBoard
   };
 }();
 
-const renderBoard = function() {
-  const BOARD = document.querySelector('#gamePad');
-
-  let fillBoard = function(arr) {
-    BOARD.innerHTML = '';
-    for  (let i = 0; i < arr.length; i++) {
-      let item = document.createElement('div');
-      item.id = [i];
-      item.innerText = arr[i];
-      item.onclick = putSign.bind(null, item.id);
-      BOARD.appendChild(item);
-    }
-  }(Gameboard.gameBoardArr);  
-
-};
-
-renderBoard();
-
-function putSign(id) {
-  if (Gameboard.gameBoardArr[id] === '') {    
-    Gameboard.gameBoardArr[id] = 'X';
-    renderBoard();
-  }
-}
+Gameboard.renderBoard();
